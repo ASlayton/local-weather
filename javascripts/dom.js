bf83aa1c21ab0f9f98f26499dc86af4a431123d7
Amanda Slayton <alslayton86@gmail.com>
2018-05-24 21:08:05 -0500

    added temp toggle

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,13 +1,17 @@
+const convert = require('./convert');
+
 const createTile = (data) => {
   let myString = '';
   const weatherIcon = data.weather[0].icon;
+  const myTemp = convert(data.main.temp);
+
   myString += `<div class="col-sm-6 col-md-4">`;
   myString += `<div class="thumbnail">`;
   myString +=   `<img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="${data.weather[0].main}">`;
   myString +=   `<div class="caption">`;
   myString +=     `<h3>${data.name}</h3>`;
   myString +=     `<p>${data.weather[0].main}</p>`;
-  myString +=     `<p>Temperature: ${data.main.temp}</p>`;
+  myString +=     `<p>Temperature: ${myTemp}</p>`;
   myString +=     `<p>Pressure: ${data.main.pressure}</p>`;
   myString +=     `<p>Wind Speed: ${data.wind.speed}</p>`;
   myString +=     `<a>View 5 day forcast</a>`;
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,11 @@ const zipValidator = require('./zipcode');
 const promises = require('./promise');
 
 const initializer = () => {
+  $('#toggle-one').bootstrapToggle({
+    on: '&deg;F',
+    off: '&deg;C',
+  });
+
   promises.retrieveKeys();
   $('#go-btn').on('click', zipValidator);
   $('#zip-input').on('keypress', keyTest);
