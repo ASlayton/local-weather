4ba7f97c079431004462f441535736a7345a1fd6
Amanda Slayton <alslayton86@gmail.com>
2018-05-24 20:12:13 -0500

    data shows up now

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,8 @@
 const zipValidator = require('./zipcode');
+const promises = require('./promise');
 
 const initializer = () => {
+  promises.retrieveKeys();
   $('#go-btn').on('click', zipValidator);
   $('#zip-input').on('keypress', keyTest);
 };
