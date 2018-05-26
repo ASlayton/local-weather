a2b414639e518f998c1b33f3ceddcea06ebaa3b4
Amanda Slayton <alslayton86@gmail.com>
2018-05-25 20:55:21 -0500

    wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -28,10 +28,41 @@ const createTile = (data) => {
   printToDom(myString);
 };
 
+const createFiveDayForcast = (dataArray) => {
+  let myString = '';
+  dataArray.forEach((data) => {
+    const weatherIcon = data.weather[0].icon;
+    const myTemp = convert(data.main.temp);
+
+    myString += `<div class="col-sm-6 col-md-4">`;
+    myString += `<div class="thumbnail">`;
+    myString +=   `<img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="${data.weather[0].main}">`;
+    myString +=   `<div class="caption">`;
+    myString +=     `<h3>${data.name}</h3>`;
+    myString +=     `<p>${data.weather[0].main}</p>`;
+
+    if ($('#toggle-one').prop('checked')) {
+      myString +=     `<p>Temperature: <span id="currentTemp" class="farenheit">${myTemp}</span>&degF;</p>`;
+    } else {
+      myString +=     `<p>Temperature: <span id="currentTemp" class="celsius">${myTemp}</span>&degC;</p>`;
+    };
+
+    myString +=     `<p>Pressure: ${data.main.pressure}</p>`;
+    myString +=     `<p>Wind Speed: ${data.wind.speed}</p>`;
+    myString +=     `<a>View 5 day forcast</a>`;
+    myString +=   `</div>`;
+    myString += `</div>`;
+    myString += `</div>`;
+  });
+
+  printToDom(myString);
+};
+
 const printToDom = (dataString) => {
   $('#weather-display-container').html(dataString);
 };
 
 module.exports = {
   createTile,
+  createFiveDayForcast,
 };
